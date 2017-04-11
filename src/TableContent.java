918c526639dcfe7b8756856f4199411d8333a657
RobinG1ll <rsgill991@gmail.com>
2017-04-11 00:13:13 -0700

    added new class for gui, changes to gui as well

---
+++ src/TableContent.java
@@ -0,0 +1,56 @@
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Tab;
+
+/**
+ * Created by Robin on 2017-04-10.
+ */
+public class TableContent {
+
+    private StringProperty source = new SimpleStringProperty();
+    private StringProperty destination = new SimpleStringProperty();
+    private IntegerProperty hitcount = new SimpleIntegerProperty();
+
+    public TableContent(String src, String des, Integer hits){
+        setSource(src);
+        setDestination(des);
+        setHitcount(hits);
+    }
+    public String getSource() {
+        return source.get();
+    }
+
+    public StringProperty sourceProperty() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source.set(source);
+    }
+
+    public String getDestination() {
+        return destination.get();
+    }
+
+    public StringProperty destinationProperty() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination.set(destination);
+    }
+
+    public int getHitcount() {
+        return hitcount.get();
+    }
+
+    public IntegerProperty hitcountProperty() {
+        return hitcount;
+    }
+
+    public void setHitcount(int hitcount) {
+        this.hitcount.set(hitcount);
+    }
+}
